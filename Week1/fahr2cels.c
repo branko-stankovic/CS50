7654cff4ff8d23e6cf363c735574d1c645b6ba04
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 22:37:37 +0100

    converting temperature from fahrenheit to celsius

--- Week1/fahr2cels.c
+++ Week1/fahr2cels.c
@@ -1,3 +1,8 @@
+/*
+*AUTHOR: Branko Stankovic
+*CONTACT: branko.stankovic@gmail.com
+*DATE CREATED: 21. December 2016.
+*/
 #include <stdio.h>
 #include <cs50.h>
 
@@ -5,6 +10,7 @@ int main(void)
 {
     printf("Please enter temperature in F: ");
     float f = GetFloat();
+    
     float c = 5.0 / 9.0 * (f - 32.0);
     printf("Temperature in C is: %.1f\n", c);
 }
