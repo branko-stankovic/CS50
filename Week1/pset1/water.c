dc14ed0a930812c2dd3552fd6496f41e89afe17e
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 21:18:48 +0100

    Update water.c

--- Week1/pset1/water.c
+++ Week1/pset1/water.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    // taking user input in minutes
+    printf("How long did you shower in minutes?\n");
+    int shower = GetInt();
+    
+    //prints the output in bottles
+    printf("Wasted bottles: %i\n", shower*192/16);
+}
