aa07a0fe10967eebcf66cef8cd74402c3f2fe4f8
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 21:11:32 +0100

    Create fahr2cels.c

---
+++ Week1/fahr2cels.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+    printf("Please enter temperature in F: ");
+    float f = GetFloat();
+    float c = 5.0 / 9.0 * (f - 32.0);
+    printf("Temperature in C is: %.1f\n", c);
+}
