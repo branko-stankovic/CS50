d2f07ab2aa5f657e014092ccb8888ad63304dcae
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 21:17:28 +0100

    Create switch.c

---
+++ Week1/switch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+    printf("Please enter an integer between 1 and 10:");
+    int x = GetInt();
+    
+    switch(x)
+    {
+        case 1:
+        case 2:
+        case 3:
+            printf("You picked a small number!");
+            break;
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            printf("You picked a medium number!");
+            break;
+        case 8:
+        case 9:
+        case 10:
+            printf("You picked a large number!");
+            break;
+        default:
+            printf("Your argument is invalid, please try again!");
+    }
+    
+    
+}
