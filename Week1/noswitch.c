0c2e15bb053b393a9bebab02f568dd1110cc5a63
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 22:40:22 +0100

    No switch allowed

--- Week1/noswitch.c
+++ Week1/noswitch.c
@@ -1,3 +1,8 @@
+/*
+*AUTHOR: Branko Stankovic
+*CONTACT: branko.stankovic@gmail.com
+*DATE CREATED: 21. December 2016.
+*/
 #include <stdio.h>
 #include <cs50.h>
 
@@ -18,6 +23,4 @@ int main(void)
     else {
         printf("Your argument is invalid, please try again!");
     }
-    
-    
 }
