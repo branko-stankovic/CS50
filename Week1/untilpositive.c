eb53d20f6db713387e352b3e9c0de851ee60f787
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 21:17:53 +0100

    Create untilpositive.c

---
+++ Week1/untilpositive.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+    int n;
+    do {
+    printf("Please enter a positive number: ");
+    n = GetInt();
+    
+    }
+    while (n<1);
+    
+    
+}
