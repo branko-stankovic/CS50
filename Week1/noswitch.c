cdf5bd8c1a993bca8106630974de11296e06ed4a
Branko Stankovic <branko.stankovic@gmail.com>
2016-12-22 21:16:18 +0100

    Create noswitch.c

---
+++ Week1/noswitch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+    printf("Please enter an integer between 1 and 10:");
+    int x = GetInt();
+    
+    if (x>0 && x<4) {
+        printf("You picked a small number!");
+    }
+    else if (x>3 && x<8) {
+        printf("You picked a medium number!");
+    }
+    else if (x>7 && x<11) {
+        printf("You picked a large number!");
+    }
+    else {
+        printf("Your argument is invalid, please try again!");
+    }
+    
+    
+}
