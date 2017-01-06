e4a0dd2770c064300ad40673024782c2c9ede853
branko-stankovic <branko.stankovic@gmail.com>
2017-01-06 19:31:24 +0100

    initials.c
    takes input through the command line, sadly

--- Week2/pset2/initials.c
+++ Week2/pset2/initials.c
@@ -1,12 +1,17 @@
 /*AUTHOR: Branko Stankovic
 CONTACT: branko.stankovic@gmail.com
 WEBPAGE: http://meanwebdeveloper.wordpress.com
-DATE CREATED: January 6th 2017 */
+DATE CREATED: January 6th 2017 
+DATE MODIFIED: January 6th 2017 */
 
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    
+    for (int i=1; i<argc; i++) 
+    {
+        printf("%c", toupper(argv[i][0]);
+    }
 }
